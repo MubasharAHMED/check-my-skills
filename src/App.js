cf5dde12bf30a573431855f9641e2dc7c663ece7
Mubashar <49880764+MubasharAHMED@users.noreply.github.com>
2021-04-29 08:05:45 +0200

    addItem operationnele: ajout d'un produit effectif avec remise à zero des valeurs.

--- src/App.js
+++ src/App.js
@@ -59,6 +59,16 @@ function App() {
 
     // code here
     // don't forget to clear the input values after adding an item to the list
+    const newShoppingList=[...shoppingList];
+    newShoppingList.push({title:currentItem,quantity:currentNumber});
+    
+    setShoppingList(newShoppingList);
+    
+    console.log("A new item is added");
+
+    //After adding:
+    setCurrentNumber(0);
+    setCurrentItem("");
   };
 
   const removeItem = (index) => {
